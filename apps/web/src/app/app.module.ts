06063cdc507d29a15dea9513cd3ff6746f653487
hieugiabao <hieugiabao12@gmail.com>
2022-12-05 22:46:22 +0700

    web: add primeng and nswag

--- apps/web/src/app/app.module.ts
+++ apps/web/src/app/app.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { ButtonModule } from 'primeng/button';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, ButtonModule],
   providers: [],
   bootstrap: [AppComponent],
 })
