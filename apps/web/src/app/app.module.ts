c887dc336e39e5d06f1338257332d1d23d57b4f6
hieugiabao <hieugiabao12@gmail.com>
2023-12-26 13:54:04 +0700

    fix build

--- apps/web/src/app/app.module.ts
+++ apps/web/src/app/app.module.ts
@@ -4,7 +4,7 @@ import { WebFeatureShellModule } from '@nx-post/web/feature-shell';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { API_BASE_URL } from '@nx-post/web/shared-data-access-api-sdk';
-import { environment } from '../enviroments/enviroment';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [AppComponent],
